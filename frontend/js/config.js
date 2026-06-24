525f0df521a590cf350f5f564ec8872cf9518b88
ManojKumarMotakatla <mightymanoj9@gmail.com>
2026-06-24 19:32:36 +0530

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,4 +2,19 @@
     "use strict";
 
     window.API_BASE = "https://tempdhas.onrender.com";
+
+    window.getAuthHeaders = function(extraHeaders = {}) {
+        const token = localStorage.getItem("dhas_token");
+
+        const headers = {
+            ...extraHeaders
+        };
+
+        if (token) {
+            headers["Authorization"] = "Bearer " + token;
+        }
+
+        return headers;
+    };
+
 })();
